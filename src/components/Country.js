17d99dacfb46933a289d723b625aa6b7e8a4d11c
Yunior Batista <yuniorbatista1113@gmail.com>
2023-03-06 10:26:11 -0500

    Breakpoints added to Country.js

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,33 +3,76 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
 
 const Country = ({ country }) => {
-  const { name, emoji, capital, code } = country[0];
+  const { name, emoji, capital, code, currency, continent } = country[0];
   const [flagSource, setFlagSource] = useState("");
   const flagUrl = "https://www.worldometers.info/img/flags/";
 
   useEffect(() => {
-    console.log("country received is: ", country[0].code);
     setFlagSource(flagUrl + code);
   }, [code, country]);
 
-  console.log(flagSource);
   return (
-    <Card sx={{ width: 375, height: 400, margin: "auto" }}>
+    <Card>
       <CardMedia
-        sx={{ width: 375, height: 200 }}
+        sx={{ width: 300, height: 200, objectFit: "cover" }}
         image={`${flagSource.toLowerCase()}-flag.gif`}
         title={`${code} flag image`}
       />
       <CardContent>
-        <Typography>
-          <strong>Country:</strong> {name}
-        </Typography>
-        <Typography>
-          <strong>Capital:</strong> {capital}
-        </Typography>
-        <Typography>Country: {name}</Typography>
+        <List space={2}>
+          <ListItem
+            alignItems="flex-start"
+            style={{ display: "flex", justifyContent: "space-evenly" }}
+          >
+            <listItemText>
+              <Typography textAlign="center">
+                <label>
+                  <strong>Country:</strong>
+                </label>
+                <br />
+                {name}
+              </Typography>
+            </listItemText>
+            <listItemText>
+              <Typography textAlign="center">
+                <label>
+                  <strong>Capital:</strong>
+                </label>
+                <br />
+                {capital}
+              </Typography>
+            </listItemText>
+          </ListItem>
+          <Divider />
+          <ListItem
+            alignItems="flex-start"
+            style={{ display: "flex", justifyContent: "space-evenly" }}
+          >
+            <listItemText>
+              <Typography textAlign="center">
+                <label>
+                  <strong>Continent:</strong>
+                </label>
+                <br />
+                {continent.name}
+              </Typography>
+            </listItemText>
+            <listItemText>
+              <Typography textAlign="center">
+                <label>
+                  <strong>Currency:</strong>
+                </label>
+                <br />
+                {currency}
+              </Typography>
+            </listItemText>
+          </ListItem>
+        </List>
       </CardContent>
     </Card>
   );
